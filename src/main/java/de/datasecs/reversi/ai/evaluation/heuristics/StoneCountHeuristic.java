de1809be1e8e6982a5ea275bcfd38b9911c29cb3
DataSecs <datasecs@outlook.de>
2020-09-26 01:20:42 +0200

    Add general heuristics.

---
+++ src/main/java/de/datasecs/reversi/ai/evaluation/heuristics/StoneCountHeuristic.java
@@ -0,0 +1,26 @@
+package de.datasecs.reversi.ai.evaluation.heuristics;
+
+import de.datasecs.reversi.map.Map;
+import de.datasecs.reversi.util.MapUtil;
+
+public class StoneCountHeuristic implements Heuristic {
+    
+    @Override
+    public void initHeuristic(Map map) {
+        int numberOfHoles = 0;
+        for (int y = 0; y < Map.getMapHeight(); y++) {
+            for (int x = 0; x < Map.getMapWidth(); x++) {
+                if (MapUtil.isTileHole(map.getGameField()[y][x])) {
+                    numberOfHoles++;
+                }
+            }
+        }
+
+        Map.setNumberOfHoles(numberOfHoles);
+    }
+
+    @Override
+    public double executeHeuristic(Map map, char player, boolean allowOverrideStones) {
+        return map.getNumberOfStones()[MapUtil.playerToInt(player)] / (double) (Map.getMapHeight() * Map.getMapWidth() - Map.getNumberOfHoles());
+    }
+}
---
+++ src/main/java/de/datasecs/reversi/ai/evaluation/heuristics/StoneParityHeuristic.java
@@ -0,0 +1,23 @@
+package de.datasecs.reversi.ai.evaluation.heuristics;
+
+import de.datasecs.reversi.map.Map;
+
+import java.util.Arrays;
+
+public class StoneParityHeuristic implements Heuristic {
+
+    @Override
+    public void initHeuristic(Map map) {}
+
+    @Override
+    public double executeHeuristic(Map map, char player, boolean allowOverrideStones) {
+        int coinParity = 0;
+        int[] num = map.getNumberOfStones();
+
+        for (int j = 1; j < num.length; j++) {
+            coinParity += (num[player] - num[j]);
+        }
+
+        return coinParity / ((double) Arrays.stream(num).sum() * num.length);
+    }
+}
