209a701d847c96828934be8382b5dbae146a7bf5
DataSecs <datasecs@outlook.de>
2021-03-10 20:17:21 +0100

    Make the statistics printing pretty.

--- src/main/java/de/marcluque/reversi/util/StatisticsUtil.java
+++ src/main/java/de/marcluque/reversi/util/StatisticsUtil.java
@@ -1,7 +1,6 @@
-package de.datasecs.reversi.util;
+package de.marcluque.reversi.util;
 
-import de.datasecs.reversi.ai.search.AbstractSearch;
-import de.datasecs.reversi.map.Map;
+import de.marcluque.reversi.ai.search.AbstractSearch;
 
 public class StatisticsUtil {
 
@@ -13,27 +12,34 @@ public class StatisticsUtil {
 
     private static final String PREFIX = "%-40s" + ANSI_RED;
 
-    public static void printStatistics(int depth, Move chosenMove, int totalStates, int leafStates, double branching,
-                                       long totalTimeForDepth, double totalTime) {
+    public static void printStatistics(int depth, Move chosenMove, int totalStates, int leafStates,
+                                       double averageBranchingForDepth, double meanBranchingFactor,
+                                       long totalTimeUntilDepth, double totalTime) {
         System.out.printf("= = = = = = = = = = = = = =" + ANSI_BLUE
                 + " STATISTICS DEPTH %d " + ANSI_RESET + "= = = = = = = = = = = = = =%n", depth);
         System.out.printf(PREFIX + "%s %s: %s%n" + ANSI_RESET, "Chosen Move:", "Player", AbstractSearch.MAX, chosenMove);
         System.out.printf(PREFIX + "%d, %d, %d%n" + ANSI_RESET, "Total-, Interior-, Leaf-nodes:",
                 totalStates, (totalStates - leafStates), leafStates);
-        System.out.printf(PREFIX + "%s%n" + ANSI_RESET, "Branching factor:", branching);
+        if (depth >= 2) {
+            System.out.printf(PREFIX + "%s%n" + ANSI_RESET, "Branching factor at depth " + (depth - 1) + ":",
+                    averageBranchingForDepth);
+        }
+        System.out.printf(PREFIX + "%s%n" + ANSI_RESET, "Mean branching factor (total states / leaf states):",
+                meanBranchingFactor);
         System.out.printf(PREFIX + "%s%n" + ANSI_RESET, "Computation time per state (ms):", totalTime / totalStates);
         System.out.printf(PREFIX + "%s%n" + ANSI_RESET, "Computation time per state (µs):",
                 (totalTime / totalStates) * 1_000);
-        System.out.printf(PREFIX + "%d%n" + ANSI_RESET, String.format("Computation time depth %d (ms):", depth),
-                totalTimeForDepth);
+        System.out.printf(PREFIX + "%d%n" + ANSI_RESET, String.format("Computation time up to (including) depth %d (ms):", depth),
+                totalTimeUntilDepth);
         System.out.printf(PREFIX + "%s%n" + ANSI_RESET, "Total computation time (ms):", totalTime);
     }
 
     public static void printStatisticsWithEstimation(int depth, Move chosenMove, int totalStates, int leafStates,
-                                                     double branching, double totalTime, long timeLeft,
-                                                     double estimatedTime, long totalTimeForDepth,
-                                                     double averageBranching) {
-        printStatistics(depth, chosenMove, totalStates, leafStates, branching, totalTimeForDepth, totalTime);
+                                                     double meanBranchingFactor, double totalTime, long timeLeft,
+                                                     double estimatedTime, long totalTimeUntilDepth,
+                                                     double averageBranchingForDepth, double averageBranching) {
+        printStatistics(depth, chosenMove, totalStates, leafStates, averageBranchingForDepth, meanBranchingFactor,
+                totalTimeUntilDepth, totalTime);
         System.out.println();
         System.out.printf("- - - - - - - - - - - - - -" + ANSI_BLUE
                 + " ESTIMATION DEPTH %d " + ANSI_RESET + "- - - - - - - - - - - - - -%n", depth + 1);
