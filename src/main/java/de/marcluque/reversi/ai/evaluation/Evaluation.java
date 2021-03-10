b29eb2814c96aca2351a48d18dfbbfc12c843492
DataSecs <datasecs@outlook.de>
2021-03-10 20:15:33 +0100

    Rework evaluation to include weights and separate building and bombing phase heuristics.

--- src/main/java/de/marcluque/reversi/ai/evaluation/Evaluation.java
+++ src/main/java/de/marcluque/reversi/ai/evaluation/Evaluation.java
@@ -1,26 +1,39 @@
-package de.datasecs.reversi.ai.evaluation;
+package de.marcluque.reversi.ai.evaluation;
 
-import de.datasecs.reversi.ai.evaluation.heuristics.Heuristic;
-import de.datasecs.reversi.map.Map;
+import de.marcluque.reversi.ai.evaluation.heuristics.Heuristic;
+import de.marcluque.reversi.map.Map;
 
 import java.util.LinkedList;
 import java.util.List;
 
 public class Evaluation {
 
-    private static final List<Heuristic> heuristics = new LinkedList<>();
+    private static final List<Heuristic> BUILDING_HEURISTICS = new LinkedList<>();
 
-    public void addHeuristic(Heuristic heuristic) {
-        heuristics.add(heuristic);
+    private static final List<Heuristic> BOMBING_HEURISTICS = new LinkedList<>();
+
+    public static void addBuildingHeuristic(Heuristic heuristic) {
+        BUILDING_HEURISTICS.add(heuristic);
     }
 
-    public void initHeuristics(Map map) {
-        heuristics.forEach(heuristic -> heuristic.initHeuristic(map));
+    public static void addBombingHeuristic(Heuristic heuristic) {
+        BOMBING_HEURISTICS.add(heuristic);
     }
 
-    public static double utility(Map map, char player) {
-        return heuristics.stream()
-                .mapToDouble(heuristic -> heuristic.executeHeuristic(map, player))
-                .sum();
+    public static void initHeuristics(Map map) {
+        BUILDING_HEURISTICS.forEach(heuristic -> heuristic.initHeuristic(map));
+        BOMBING_HEURISTICS.forEach(heuristic -> heuristic.initHeuristic(map));
+    }
+
+    public static double utility(Map map) {
+        if (Map.getPhase() == 1) {
+            return BUILDING_HEURISTICS.stream()
+                    .mapToDouble(heuristic -> heuristic.executeHeuristic(map))
+                    .sum();
+        } else {
+            return BOMBING_HEURISTICS.stream()
+                    .mapToDouble(heuristic -> heuristic.executeHeuristic(map))
+                    .sum();
+        }
     }
 }
--- src/main/java/de/marcluque/reversi/ai/evaluation/rules/Rules.java
+++ src/main/java/de/marcluque/reversi/ai/evaluation/rules/Rules.java
@@ -1,12 +1,22 @@
-package de.datasecs.reversi.ai.evaluation.rules;
+package de.marcluque.reversi.ai.evaluation.rules;
+
+import de.marcluque.reversi.map.Map;
+import de.marcluque.reversi.util.MapUtil;
 
 public class Rules {
 
     public static boolean OVERRIDE_STONES = false;
 
-    public static boolean ;
+    public static void updateOverrideStoneRule(Map map) {
+        int freeTiles = 0;
+        for (int y = 0; y < Map.getMapHeight(); y++) {
+            for (int x = 0; x < Map.getMapWidth(); x++) {
+                if (!MapUtil.isOccupied(map.getGameField()[y][x])) {
+                    freeTiles++;
+                }
+            }
+        }
 
-    public static void updateRules() {
-
+        OVERRIDE_STONES = freeTiles - Map.getNumberOfHoles() == 0;
     }
 }
