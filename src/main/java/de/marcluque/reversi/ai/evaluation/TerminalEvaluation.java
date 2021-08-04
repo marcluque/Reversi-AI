c864a0fad7c62180e4e8a91d66c2f5628562b69d
marcluque <marc.luque@outlook.com>
2021-08-05 01:48:46 +0200

    Update terminal evaluation.

--- src/main/java/de/marcluque/reversi/ai/evaluation/TerminalEvaluation.java
+++ src/main/java/de/marcluque/reversi/ai/evaluation/TerminalEvaluation.java
@@ -4,18 +4,15 @@ import de.marcluque.reversi.ai.search.AbstractSearch;
 import de.marcluque.reversi.map.Map;
 import de.marcluque.reversi.util.MapUtil;
 
+import java.util.Arrays;
+
 /*
  * Created with <3 by marcluque, March 2021
  */
 public class TerminalEvaluation {
 
     public static double utility(Map map) {
-        // Find player with max number of stones
-        int playerWithMostStones = MapUtil.playerWithMaxStones(map);
-        double maxPlayerStones = map.getNumberOfStones()[AbstractSearch.MAX_NUMBER];
-
-        // Return 1 for first place in ranking and lowest value for last place in ranking
-        return maxPlayerStones / map.getNumberOfStones()[playerWithMostStones];
+        return (double) map.getNumberOfStones()[AbstractSearch.MAX_NUMBER] / Arrays.stream(map.getNumberOfStones()).sum();
     }
 
     public static double[] nPlayerUtility(Map map) {
