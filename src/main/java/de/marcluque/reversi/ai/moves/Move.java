a8d88e372c435441d1380daae993493e30cbe9e5
Marc Luqué <marc.luque@outlook.com>
2022-07-23 19:26:57 +0200

    Make some more improvements to build move validity check

--- src/main/java/de/marcluque/reversi/ai/moves/Move.java
+++ src/main/java/de/marcluque/reversi/ai/moves/Move.java
@@ -44,6 +44,7 @@ public abstract class Move {
     private static boolean isMoveValidImpl(Map map, int x, int y, char player, boolean returnEarly,
                                            boolean allowOverrideStones, List<Coordinate> capturableTiles) {
         char currentTile = map.getGameField()[y][x];
+        int playerNumber = MapUtil.playerToInt(player);
         // Holes are not allowed, neither for building nor for bomb phase
         if (MapUtil.isTileHole(currentTile)) {
             return false;
@@ -52,7 +53,7 @@ public abstract class Move {
         else if (Map.getPhase() == 1) {
             // Tile may be occupied by player or expansion stone -> override stones must be allowed and available for player
             if (MapUtil.isOccupied(currentTile)
-                    && (!allowOverrideStones || map.getOverrideStones()[Character.getNumericValue(player)] == 0)) {
+                    && (!allowOverrideStones || map.getOverrideStones()[playerNumber] == 0)) {
                 return false;
             }
 
@@ -83,7 +84,7 @@ public abstract class Move {
         } else {
             // Elimination phase
             // Check basic invalidity for elimination phase
-            return map.getBombs()[Character.getNumericValue(player)] > 0;
+            return map.getBombs()[playerNumber] > 0;
         }
     }
 
@@ -97,8 +98,7 @@ public abstract class Move {
 
         do {
             Transition transitionEnd = Map.getTransitions().get(new Transition(x, y, direction));
-
-            // Follow the transition, if there is one and adapt its direction
+            // Follow the transition (if there is one) and adapt its direction
             if (transitionEnd != null) {
                 // Jump to stone the transitions ends on
                 x = transitionEnd.getX();
@@ -110,22 +110,19 @@ public abstract class Move {
                 y += CORNERS[direction][1];
             }
 
+            // Stop if we make a 'loop' (i.e., we see a tile that we have seen before)
             if (!tempTiles.add(new Coordinate(x, y))) {
                 break;
-            } else {
-                pathLength += 1;
             }
+
+            pathLength += 1;
         } while (MapUtil.isCoordinateInMap(x, y) && MapUtil.isCapturableStone(map, x, y, player));
 
         // Check whether the last tile of the path is in the map, not the start tile and has the player stone on it
-        if (!(MapUtil.isCoordinateInMap(x, y)
+        return MapUtil.isCoordinateInMap(x, y)
                 && pathLength > 0
                 && map.getGameField()[y][x] == player
-                && (startX != x || startY != y))) {
-            tempTiles = new HashSet<>();
-        }
-
-        return !tempTiles.isEmpty();
+                && (startX != x || startY != y);
     }
 
     public static MoveTriplet executeMove(Map map, int x, int y, int specialField, char player, List<Coordinate> capturableStones) {
