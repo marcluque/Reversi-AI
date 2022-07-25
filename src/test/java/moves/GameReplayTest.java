05db057f6a176c03151f593fe5e146a880e2e95b
Marc Luqué <marc.luque@outlook.com>
2022-07-25 15:52:01 +0200

    Add surefire plugin to build

--- src/test/java/moves/GameReplayTest.java
+++ src/test/java/moves/GameReplayTest.java
@@ -9,6 +9,7 @@ import de.marcluque.reversi.util.MapUtil;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
